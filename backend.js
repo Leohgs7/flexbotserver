4cfea4804a3bd7b778c9f7328997a7c0c593082f
Leandro Henrique <107160517+Leohgs7@users.noreply.github.com>
2025-08-20 18:39:03 -0300

    gemini 2.5 flash lite

--- backend.js
+++ backend.js
@@ -106,7 +106,7 @@ app.post('/api/chatbot', async (req, res) => {
         
         // Chama a IA com o prompt enriquecido
         const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-        const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
         const result = await model.generateContent(finalPrompt);
         const response = await result.response;
         const text = response.text();
